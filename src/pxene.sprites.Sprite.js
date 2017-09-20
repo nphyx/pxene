5b43806e02e66d614603f99f9eae20272ba1cc3b
Justen Robertson <justen@justen.us>
2017-09-19 20:30:47 -0700

    Add auto-generated horizontal flip for sprite sheets.

--- src/pxene.sprites.Sprite.js
+++ src/pxene.sprites.Sprite.js
@@ -17,17 +17,40 @@ export default function Sprite(frameCount, frameWidth, frameHeight, animations, 
 	return this;
 }
 
+Sprite.prototype.generateFlipped = function generateFlipped() {
+	let canvas = document.createElement("canvas");
+	canvas.width = this.image.width;
+	canvas.height = this.image.height;
+	let context = canvas.getContext("2d");
+	this.flipped = canvas;
+	let row, col, sx, sy, dx, dy;
+	let rows = this.rows;
+	let cols = this.cols;
+	let w = this.frameWidth;
+	let h = this.frameHeight;
+
+	context.scale(-1, 1);
+	for(row = 0, rows = this.rows; row < rows; ++row) {
+		for(col = 0, cols = this.columns; col < cols; ++col) {
+			sx = col * w;
+			dx = sx; //((cols - col) * w) - w;
+			sy = dy = row * h;
+			context.drawImage(this.image, sx, sy, w, h, -sx-w, dy, w, h);
+		}
+	}
+	context.setTransform(1, 0, 0, 1, 0, 0);
+}
+
 Sprite.prototype.load = function load() {
 	if(!this.ready) {
 		return new Promise(resolve => {
 			assets.getAsset(this.imageURI).then(image => {
-				image.content.addEventListener("load", () => {
-					this.ready = true;
-					this.image = image.content;
-					this.columns = image.content.width / this.frameWidth;
-					this.rows = image.content.height / this.frameHeight;
-					resolve(this);
-				});
+				this.ready = true;
+				this.image = image.content;
+				this.columns = image.content.width / this.frameWidth;
+				this.rows = image.content.height / this.frameHeight;
+				this.generateFlipped();
+				resolve(this);
 			});
 		});
 	}
@@ -41,8 +64,9 @@ Sprite.prototype.load = function load() {
  * @param {int} frame the frame number to display
  * @param {CanvasRenderingContext2D} context a context to draw to
  * @param {vec2} pos the top left corner from which to start drawing
+ * @param {bool} flip horizontal flip toggle (to reverse facing of sprite)
  */
-Sprite.prototype.animate = function animate(name, frame, context, pos) {
+Sprite.prototype.animate = function animate(name, frame, context, pos, flip = false) {
 	let animation = (
 			this.animations[name]?
 			this.animations[name]:
@@ -50,7 +74,7 @@ Sprite.prototype.animate = function animate(name, frame, context, pos) {
 	let frameNum = animation.startFrame + (frame % animation.length);
 	let {frameWidth, frameHeight} = this;
 	context.drawImage(
-		this.image, 
+		flip?this.flipped:this.image, 
 		getX(this, frameNum), getY(this, frameNum),
 		frameWidth, frameHeight,
 		pos[0], pos[1], 
